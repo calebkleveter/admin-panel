d2825ff618be4f620ceb70e54854df5e6c71aca3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-12 15:43:17 -0500

    Sources/AdminPanel/Controllers/DashboardController.swift - Created Dashboard.customRoute{Paths && URLs}

--- Sources/AdminPanel/Controllers/DashboardController.swift
+++ Sources/AdminPanel/Controllers/DashboardController.swift
@@ -5,9 +5,22 @@ import Auth
 public final class DashboardController {
     
     public let drop: Droplet
+    public let customRoutePaths: [String]?
+    public var customRouteURLs: [String]?
     
-    public init(droplet: Droplet) {
+    public init(droplet: Droplet, customViews views: [String]?) {
         drop = droplet
+        customRoutePaths = views
+        
+        if let paths = self.customRoutePaths {
+            var urls: [String] = []
+            for path in paths {
+                if let fileName = path.characters.split(separator: "/").map(String.init).last {
+                    urls.append(fileName)
+                }
+            }
+            customRouteURLs = urls
+        }
     }
     
     public func index(request: Request) throws -> ResponseRepresentable {
