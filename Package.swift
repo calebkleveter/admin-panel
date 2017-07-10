9bafe54d104c9eb88ba63e151b53f2e8f1a29719
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-10 12:49:03 -0500

    Updated forms package url

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/nodes-vapor/sugar.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor-community/forms.git", majorVersion: 0),
+        .Package(url: "https://github.com/calebkleveter/forms.git", majorVersion: 0),
         .Package(url: "https://github.com/nodes-vapor/slugify.git", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/flash.git", majorVersion: 1),
         .Package(url: "https://github.com/nodes-vapor/paginator.git", majorVersion: 1),
